bb63f726f33d792b2a7547b54bac7abd7aad3af3
Dmitriy Derepko <xepozz@list.ru>
2025-10-05 10:54:21 +0400

    chore: initial setup

--- src/main/kotlin/com/github/xepozz/moonshine/MoonshineBundle.kt
+++ src/main/kotlin/com/github/xepozz/moonshine/MoonshineBundle.kt
@@ -1,13 +1,13 @@
-package com.github.xepozz.moonshineplugin
+package com.github.xepozz.moonshine
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
 import org.jetbrains.annotations.PropertyKey
 
 @NonNls
-private const val BUNDLE = "messages.MyBundle"
+private const val BUNDLE = "messages.Moonshine"
 
-object MyBundle : DynamicBundle(BUNDLE) {
+object MoonshineBundle : DynamicBundle(BUNDLE) {
 
     @JvmStatic
     fun message(@PropertyKey(resourceBundle = BUNDLE) key: String, vararg params: Any) =
