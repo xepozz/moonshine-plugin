4a1effa20278c89db232cac2af4a578a52cd1d91
Dmitriy Derepko <xepozz@list.ru>
2025-10-08 21:54:00 +0400

    feat: add moonshine ion

---
+++ src/main/kotlin/com/github/xepozz/moonshine/MoonshineIcons.kt
@@ -0,0 +1,8 @@
+package com.github.xepozz.moonshine
+
+import com.intellij.openapi.util.IconLoader
+
+object MoonshineIcons {
+    @JvmStatic
+    val MOONSHINE = IconLoader.getIcon("/icons/moonshine/icon.svg", MoonshineIcons::class.java)
+}
