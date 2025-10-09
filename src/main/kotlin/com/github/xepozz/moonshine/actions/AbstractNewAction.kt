06ef46d28b8a03bfa63f03803c946d6a9ec6af4b
Dmitriy Derepko <xepozz@list.ru>
2025-10-09 13:35:25 +0400

    feat: show popup for not implemented actions

--- src/main/kotlin/com/github/xepozz/moonshine/actions/AbstractNewAction.kt
+++ src/main/kotlin/com/github/xepozz/moonshine/actions/AbstractNewAction.kt
@@ -1,10 +1,17 @@
 package com.github.xepozz.moonshine.actions
 
+import com.intellij.notification.NotificationGroupManager
+import com.intellij.notification.NotificationType
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 
 open class AbstractNewAction : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
-        println("Hello World!")
+        val project = e.project ?: return
+
+        NotificationGroupManager.getInstance()
+            .getNotificationGroup("MoonShine Debug")
+            .createNotification("Not implemented yet", NotificationType.INFORMATION)
+            .notify(project);
     }
 }
